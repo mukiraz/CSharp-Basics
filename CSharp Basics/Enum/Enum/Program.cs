3a3d67ed441be12ca56818f9f44ba02499eab42d
mukiraz <mukiraz@gmail.com>
2022-06-10 15:59:53 +0200

    Enum added

---
+++ CSharp Basics/Enum/Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Friday);
+            Console.ReadKey();
+        }
+
+        //Enums are used for the constants that you do not need to change.
+        enum Days
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+
+    }
+}
