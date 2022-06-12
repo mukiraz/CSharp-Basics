ccf261b4ee99fcf2ff2e88ba320dd32cd8b9b3fb
mukiraz <mukiraz@gmail.com>
2022-06-12 12:41:03 +0200

    Arrays Added

---
+++ CSharp Basics/004 Methods, Overloading, Params/Methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add(50, 60);
+            int factorial = Factorial(5);
+            
+            Console.WriteLine(factorial);
+            Console.WriteLine("The sum of the numbers are: {0}", Add(1, 2, 3, 4, 5, 6, 7, 8, 9));
+            Console.ReadKey();
+        }
+
+        // Without parameters
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        //With parameters.
+        //This is the method overloading. We have to use the same method mane with different parameters.
+        static void Add(int number1 = 20, int number2= 30)
+        {
+            int sum = number1 + number2;
+            Console.WriteLine(sum);
+        }
+
+        static int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        // This is a recursive method
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ CSharp Basics/006 Arrays/006 Arrays/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _006_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Arrays:
+
+            // We can create our array like that
+            string[] students = new string[3];
+
+            students[0] = "Alice";
+            students[1] = "Bob";
+            students[2] = "Charlie";
+
+
+
+            ShowStringElements(students);
+
+            //Or like this
+
+            int[] numbers = new int[8]
+            {
+                1,2,3,4,5,6,7,8
+            };
+
+            ShowIntElements(numbers);
+
+            string[,] regions = new string[5, 3]
+            {
+                {"İstanbul", "İzmit","Balıkesir"},
+                {"Ankara", "Konya","Kırıkkale"},
+                {"Antalya", "Adana","Mersin"},
+                {"Rize", "Trabzon","Samsun"},
+                {"İzmir", "Muğla","Manisa"}
+            };
+
+            ShowRegions(regions);
+
+            Console.ReadKey();
+
+
+        }
+
+        static void ShowStringElements(string[] elements)
+        {
+            foreach (string element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void ShowIntElements(int[] elements)
+        {
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void ShowRegions(string[,] regions)
+        {
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+
+                Console.WriteLine("******");
+
+            }
+        }
+
+
+    }
+}
