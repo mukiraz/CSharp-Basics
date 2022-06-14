9ee5b6453623240ba690d7dac4d557cb4872f395
mukiraz <mukiraz@gmail.com>
2022-06-14 14:57:03 +0200

    Chess board added, Class and Interfaces Solution Added.

---
+++ CSharp Basics/009 Classes and Interfaces/Classes/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add();
+
+            //We can create object like that
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.City = "Naters";
+            customer.FirstName = "Murat Ugur";
+            customer.LastName = "KİRAZ";
+
+            //We can create object like that also
+
+            Customer customer2 = new Customer
+            {
+                Id = 1, City = "Naters" , FirstName = "Murat Ugur", LastName = "KİRAZ"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    //Classes are used for creating objects.
+
+    class CustomerManager
+    {
+        
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Customer Deleted");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Product Updated");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Product Deleted");
+        }
+    }
+
+
+    class Customer
+    {
+        //These properties are for encapsulation.
+
+        //There is no different issue about writing like that
+        //We define private variables in the scope begins with _
+        private string _Address;
+
+        public string Adress
+        {
+            //Here the get block returns the value of private variable
+            get
+            {
+                return "The address is: "+ _Address;
+            }
+
+            //Here the set block sets the value of private variable
+            set
+            {
+                _Address = value;
+            }
+        }
+        //prop tab tab
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ CSharp Basics/010 ChessBoard/ChessBoard/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessBoard
+{
+    public partial class frmChessBoard : Form
+    {
+        public frmChessBoard()
+        {
+            InitializeComponent();
+            this.Width = 640 + 10;
+            this.Height = 640 + 10;
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This is a way
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        AddButton(i,j);
+            //    }
+
+            //}
+            //This is another way
+
+            Button[,] buttons = new Button[8,8];
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 79;
+                    buttons[i, j].Height = 79;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    left += 75;
+                    this.Controls.Add(buttons[i, j]);
+                }
+                left = 0;
+                top += 75;
+
+            }
+
+        }
+
+
+
+
+        private void AddButton(int i, int j )
+        {
+            Button button = new Button();
+            button.Height = 80;
+            button.Width = 80;
+            button.Left = i * 79;
+            button.Top = j * 79;
+            if ((i+j) % 2 == 0)
+            {
+                button.BackColor = Color.White;
+            }
+            else
+            {
+                button.BackColor = Color.Black;
+            }
+            this.Controls.Add(button);
+        }
+    }
+}
